94cd5accfd7a742def901b58031596daee575277
lheckman13 <32986608+lheckman13@users.noreply.github.com>
2022-08-02 20:36:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,12 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
 #read csv
   df1=pd.read_csv(uploaded_file)
-  st.dataframe(df1)
+  df1=df1.sample(frac=1)
+  col1=df1[1:5]
+  col2=df1[6:10]
+  col3=df1[11:15]
+  final=pd.dataframe()
+  final['B']=col1
+  final['I']=col2
+  final['N']=col3
+  st.dataframe(final, width=50, height=50)
