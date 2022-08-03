f7ad0042b32972db43dcfa7ad759b4f3e05af34f
lheckman13 <32986608+lheckman13@users.noreply.github.com>
2022-08-02 20:11:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,6 @@ import streamlit as st
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
 #read csv
-df1=pd.read_csv(uploaded_file)
+  df1=pd.read_csv(uploaded_file)
 
+st.dataframe(df1)
